8ac5e9aef65b538a051ceb0f6ea4e1eac1ab05d0
Şuayb Şimşek <suayb.simsek@detaysoft.com>
2021-01-15 10:41:03 +0300

    add cors

---
+++ src/main/java/com/spring/gridfs/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.spring.gridfs.config;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class WebConfig {
+
+   final long MAX_AGE_SECS = 3600;
+
+    @Bean
+    CorsWebFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setMaxAge(MAX_AGE_SECS);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(Arrays.asList("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
+
+}
